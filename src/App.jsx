56d4cfb70e87a5d2e74ce086d495daacaac69c9d
shaikrahimullah18-crypto <shaikrahimiullah18@gmail.com>
2026-01-03 10:10:15 +0530

    3.1.26

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div>
+    <h1>Welcome to FEDFUI class</h1>
+    <h2>2500030310-RAHIM SHAIK</h2>
+    <h3>DATE-3.1.26</h3>
+   </div>
+  )
+}
+
+export default App
